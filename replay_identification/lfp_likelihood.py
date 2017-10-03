ea2086869a6e1a1df782983d5c9e61beef9751be
edeno <edeno@bu.edu>
2017-10-03 15:05:32 -0400

    Add code for fitting speed and lap likelihoods

---
+++ replay_identification/lfp_likelihood.py
@@ -0,0 +1,72 @@
+import numpy as np
+from sklearn.neighbors import KernelDensity
+from spectral import Multitaper, Connectivity
+
+
+def lfp_likelihood_ratio(lfps, is_candidate_replay, sampling_frequency):
+    '''
+
+    Parameters
+    ----------
+    lfps : ndarray, shape (n_time, n_signals)
+    is_candidate_replay : bool ndarray, shape (n_time,)
+    sampling_frequency : float
+
+    Returns
+    -------
+    lfp_likelihood : ndarray (n_time,)
+
+    '''
+    ripple_band_power = estimate_ripple_band_power(
+        lfps, sampling_frequency)
+    kde = likelihood_kde(ripple_band_power)
+    out_replay_likelihood = kde.score_samples(np.log(
+        ripple_band_power[~is_candidate_replay]))
+    in_replay_likelihood = kde.score_samples(
+        np.log(ripple_band_power[is_candidate_replay]))
+
+    return in_replay_likelihood - out_replay_likelihood
+
+
+def likelihood_kde(ripple_band_power):
+    '''Evaluate a multivariate gaussian kernel for each time point
+
+    Parameters
+    ----------
+    ripple_band_power : ndarray, shape (n_time, n_signals)
+
+    Returns
+    -------
+    kernel_density_estimate : sklearn function
+
+    '''
+    n_time, n_signals = ripple_band_power.shape
+    ripple_band_power = np.log(ripple_band_power)
+    # replace with np.var?
+    power_variances = (np.std(ripple_band_power, axis=0) *
+                       (4 / (n_signals + 2) / n_time) **
+                       (1 / (n_signals + 4))) ** 2
+    kde = KernelDensity(kernel='gaussian', bandwidth=power_variances)
+    return kde.fit(ripple_band_power)
+
+
+def estimate_ripple_band_power(lfps, sampling_frequency):
+    '''Estimates the 200 Hz power of each LFP
+
+    Parameters
+    ----------
+    lfps : ndarray, shape (n_time, n_signals)
+    sampling_frequency : float
+
+    Returns
+    -------
+    ripple_band_power : ndarray (n_time, n_signals)
+
+    '''
+    m = Multitaper(lfps, sampling_frequency=sampling_frequency,
+                   time_halfbandwidth_product=1,
+                   time_window_duration=0.020,
+                   time_window_step=1 / sampling_frequency)
+    c = Connectivity.from_multitaper(m)
+    closest_200Hz_freq_ind = np.argmin(np.abs(c.frequencies - 200))
+    return c.power()[..., closest_200Hz_freq_ind, :].squeeze()
---
+++ replay_identification/speed_likelhood.py
@@ -0,0 +1,65 @@
+import numpy as np
+from statsmodels.api import GLM, families
+from patsy import build_design_matrices, dmatrix
+
+
+def log_likelihood(speed):
+    speed_std = np.std(speed[1:] - speed[:-1])
+    return (-np.log(speed_std) - 0.5 * (speed[1:] -
+            speed[:-1]) ** 2) / speed_std ** 2
+
+
+def estimate_speed_likelihood_ratio(speed, is_candidate_replay,
+                                    speed_threshold=4):
+    '''p(v_t|v_{t-1}, I_t)
+
+    l_vel in Long Tao's code
+
+    Parameters
+    ----------
+    speed : ndarray, shape (n_time,)
+    is_candidate_replay : boolean ndarray, shape (n_time,)
+    speed_threshold : float, optional
+
+    Returns
+    -------
+    likelihood_ratio : ndarray (n_time,)
+
+    '''
+    likelihood_ratio = (log_likelihood(speed[is_candidate_replay]) -
+                        log_likelihood(speed[~is_candidate_replay]))
+    likelihood_ratio[speed > speed_threshold] = -speed[
+        speed > speed_threshold]
+    return likelihood_ratio
+
+
+def estimate_indicator_probability(speed, is_candidate_replay):
+    '''Estimate the predicted probablity of replay given speed and whether
+    it was a replay in the previous time step.
+
+    p(I_t | I_t-1, v_t-1)
+
+    p_I_0, p_I_1 in Long Tao's code
+
+    Parameters
+    ----------
+    speed : ndarray, shape (n_time,)
+    is_candidate_replay : boolean ndarray, shape (n_time,)
+
+    Returns
+    -------
+    probability_replay : ndarray, shape (2, n_time)
+
+    '''
+
+    design_matrix = dmatrix('is_replay + bs(speed, knots=[1, 2, 3, 20])',
+                            dict(speed=speed[1:],
+                                 is_replay=is_candidate_replay))
+    fit = GLM(is_candidate_replay[1:], design_matrix,
+              family=families.Binomial()).fit()
+
+    predict_design_matrix = build_design_matrices(
+        [design_matrix.design_info],
+        dict(is_replay=np.unique(is_candidate_replay)))[0]
+    return (np.exp(np.dot(predict_design_matrix, fit.params)) /
+            (1 + np.exp(np.dot(predict_design_matrix, fit.params))))
