40a39dcdcba5abc3de66726776909ed275c1a830
edeno <edeno@bu.edu>
2017-10-06 13:24:10 -0400

    Fix logic for lip likelihood.
    Need to consider how to estimate the in-ripple lfp carefully still.

--- replay_identification/lfp_likelihood.py
+++ replay_identification/lfp_likelihood.py
@@ -19,16 +19,21 @@ def lfp_likelihood_ratio(lfps, is_candidate_replay, sampling_frequency):
     '''
     ripple_band_power = estimate_ripple_band_power(
         lfps, sampling_frequency)
-    kde = likelihood_kde(ripple_band_power)
-    out_replay_likelihood = kde.score_samples(np.log(
-        ripple_band_power[~is_candidate_replay]))
-    in_replay_likelihood = kde.score_samples(
-        np.log(ripple_band_power[is_candidate_replay]))
 
-    return in_replay_likelihood - out_replay_likelihood
+    kde_in_replay = estimate_kernel_density(
+        ripple_band_power[is_candidate_replay])
+    kde_out_replay = estimate_kernel_density(
+        ripple_band_power[~is_candidate_replay])
+
+    in_replay_log_likelihood = kde_in_replay.score_samples(
+        ripple_band_power)
+    out_replay_log_likelihood = kde_out_replay.score_samples(
+        ripple_band_power)
+
+    return in_replay_log_likelihood - out_replay_log_likelihood
 
 
-def likelihood_kde(ripple_band_power):
+def estimate_kernel_density(ripple_band_power):
     '''Evaluate a multivariate gaussian kernel for each time point
 
     Parameters
