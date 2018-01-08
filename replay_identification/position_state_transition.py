Fix normalization so axis is correct and the bin size is multiplied because it is an integral

replay_identification/position_state_transition.py
@@ -23,7 +23,7 @@ def estimate_movement_variance(position, speed, speed_threshold=4.0):
 
 
 def estimate_position_state_transition(place_bins, position, variance):
-    """Zero mean random walk with covariance based on movement
+    """Zero mean random walk with covariance based on movement.
 
     p(x_{k} | x_{k-1}, I_{k}, I_{k-1})
     """
@@ -31,15 +31,18 @@ def estimate_position_state_transition(place_bins, position, variance):
     state_transition_matrix = norm.pdf(
         place_bins[:, np.newaxis], loc=place_bins[np.newaxis, :],
         scale=variance)
-    state_transition_matrix /= np.sum(
         state_transition_matrix, axis=0, keepdims=True) / position_bin_size
 
-    return state_transition_matrix
 
-
-def fit_position_state_transition(position, speed, spikes, place_bins,
+def fit_position_state_transition(position, speed, place_bins,
                                   speed_threshold=4.0, speed_up_factor=20):
     movement_variance = estimate_movement_variance(
         position, speed, speed_threshold)
     return np.linalg.matrix_power(estimate_position_state_transition(
         place_bins, position, movement_variance), speed_up_factor)
+
+
+def _normalize_row_probability(x, binsize=1):
+    '''Ensure the state transition matrix rows sum to 1
+    '''
+    return x / (x.sum(axis=1, keepdims=True) * binsize)