40f1559e85a3c695dc1444395c69807fb53439c2
Eric Denovellis <edeno@bu.edu>
2018-01-10 11:11:37 -0500

    Add empirical movement transition matrix function
    From xinyi's replay decoding. Uses the actual movements for determining the likely transitions from one position to another.

--- replay_identification/position_state_transition.py
+++ replay_identification/position_state_transition.py
@@ -1,6 +1,7 @@
 import numpy as np
 from patsy import dmatrices
 from scipy.stats import norm
+from scipy.ndimage.filters import gaussian_filter
 from statsmodels.api import GLM, families
 from statsmodels.tsa.tsatools import lagmat
 
@@ -45,3 +46,63 @@ def _normalize_row_probability(x):
     '''Ensure the state transition matrix rows sum to 1
     '''
     return x / x.sum(axis=1, keepdims=True)
+
+
+def empirical_movement_transition_matrix(place, place_bin_edges, speed,
+                                         sequence_compression_factor=16,
+                                         is_condition=None,
+                                         speed_threshold=4.0):
+    '''Estimate the probablity of the next position based on the movement
+     data, given the movment is sped up by the
+     `sequence_compression_factor`
+
+    Place cell firing during a hippocampal replay event is a "sped-up"
+    version of place cell firing when the animal is actually moving.
+    Here we use the animal's actual movements to constrain which place
+    cell is likely to fire next.
+
+    Parameters
+    ----------
+    place : array_like, shape (n_time,)
+        Linearized position of the animal over time
+    place_bin_edges : array_like, shape (n_bins,)
+    sequence_compression_factor : int, optional
+        How much the movement is sped-up during a replay event
+    is_condition : array_like, shape (n_time,)
+        Boolean indicator for an experimental condition.
+    Returns
+    -------
+    empirical_movement_transition_matrix : array_like,
+                                           shape=(n_bin_edges-1,
+                                           n_bin_edges-1)
+
+    '''
+    place = np.array(place)
+    movement_variance = estimate_movement_variance(
+        place, speed, speed_threshold)
+
+    if is_condition is None:
+        is_condition = np.ones_like(place, dtype=bool)
+
+    place = np.stack((place[1:], place[:-1]))
+    place = place[:, is_condition[1:]]
+
+    movement_bins, _, _ = np.histogram2d(place[0], place[1],
+                                         bins=(place_bin_edges,
+                                               place_bin_edges),
+                                         normed=False)
+
+    smoothed_movement_bins_probability = gaussian_filter(
+        _normalize_row_probability(
+            _fix_zero_bins(movement_bins)), sigma=movement_variance)
+    return np.linalg.matrix_power(
+        smoothed_movement_bins_probability,
+        sequence_compression_factor)
+
+
+def _fix_zero_bins(movement_bins):
+    '''If there is no data observed for a column, set everything to 1 so
+    that it will have equal probability
+    '''
+    movement_bins[:, movement_bins.sum(axis=0) == 0] = 1
+    return movement_bins
