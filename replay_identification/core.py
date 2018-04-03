fa45109d1756397dc04258b3fd54472847465e4b
Eric Denovellis <edeno@bu.edu>
2018-04-03 12:12:33 -0400

    Remove unused function

--- replay_identification/core.py
+++ replay_identification/core.py
@@ -2,32 +2,6 @@ import numpy as np
 from functools import wraps
 
 
-def scaled_likelihood(log_likelihood_func):
-    '''Converts a log likelihood to a scaled likelihood with its max value at
-    1.
-
-    Used primarily to keep the likelihood numerically stable because more
-    observations at a time point will lead to a smaller overall likelihood
-    and this can exceed the floating point accuarcy of a machine.
-
-    Parameters
-    ----------
-    log_likelihood_func : function
-
-    Returns
-    -------
-    scaled_likelihood : function
-
-    '''
-    @wraps(log_likelihood_func)
-    def decorated_function(*args, **kwargs):
-        log_likelihood = log_likelihood_func(*args, **kwargs)
-        axis = tuple(range(log_likelihood.ndim))[1:]
-        return np.exp(log_likelihood - np.max(log_likelihood, axis=axis))
-
-    return decorated_function
-
-
 def combined_likelihood(log_likelihood_function):
     @wraps(log_likelihood_function)
     def decorated_function(*args, **kwargs):
