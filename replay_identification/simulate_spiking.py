08c815bdf773a0d56b7caae96030d4e5b105775b
Eric Denovellis <edeno@bu.edu>
2018-04-12 13:25:41 -0400

    Move multiunit simulation to its own module

--- replay_identification/simulate_spiking.py
+++ replay_identification/simulate_spiking.py
@@ -1,6 +1,6 @@
 '''Tools for simulating place field spiking'''
 import numpy as np
-from scipy.stats import norm, multivariate_normal
+from scipy.stats import norm
 
 
 def simulate_time(n_samples, sampling_frequency):
@@ -49,29 +49,3 @@ def create_place_field(
     field_firing_rate /= field_firing_rate.max()
     field_firing_rate[~is_condition] = 0
     return baseline_firing_rate + max_firing_rate * field_firing_rate
-
-
-def generate_marks(spikes, mark_mean, mark_std_deviation, n_marks=4):
-    '''Generate a place field with an associated mark'''
-    spikes[spikes == 0] = np.nan
-    marks = multivariate_normal(
-        mean=[mark_mean] * n_marks,
-        cov=[mark_std_deviation] * n_marks).rvs(size=(spikes.size,))
-    return marks * spikes[:, np.newaxis]
-
-
-def simulate_multiunit(
-    place_field_means, mark_means, linear_distance, sampling_frequency,
-        mark_std_deviation=20, n_marks=4, **kwargs):
-    '''Simulate a single tetrode assuming each tetrode picks up several
-    neurons with different place fields with distinguishing marks.'''
-    unit = []
-    for place_field_mean, mark_mean in zip(place_field_means, mark_means):
-        rate = create_place_field(
-            place_field_mean, linear_distance, sampling_frequency, **kwargs)
-        spikes = simulate_poisson_spikes(rate, sampling_frequency)
-        marks = generate_marks(
-            spikes, mark_mean, mark_std_deviation, n_marks=n_marks)
-        unit.append(marks)
-
-    return np.nanmean(np.stack(unit, axis=0), axis=0)
