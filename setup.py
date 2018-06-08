8f4c83d16978c1ebf45a76bb31ffa381d8c60882
Eric Denovellis <edeno@bu.edu>
2018-06-08 11:33:27 -0400

    Bump version number

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ TESTS_REQUIRE = ['pytest >= 2.7.1']
 
 setup(
     name='replay_identification',
-    version='0.1.5.dev0',
+    version='0.1.6.dev0',
     license='MIT',
     description=('Identify replay events using multiple information sources'),
     author='Eric Denovellis',
