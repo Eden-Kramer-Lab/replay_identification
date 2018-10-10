74cd9109aee278b65680796b0c832b82b3e0e069
Eric Denovellis <edeno@bu.edu>
2018-10-10 11:19:37 -0400

    Make function more generic

--- replay_identification/core.py
+++ replay_identification/core.py
@@ -1,14 +1,13 @@
 import numpy as np
 
 
-def get_bin_edges(position, n_bins=None, place_bin_size=None):
-    not_nan_position = position[~np.isnan(position)]
-    if place_bin_size is not None:
-        n_bins = (np.round(np.ceil(np.ptp(not_nan_position) / place_bin_size))
-                  ).astype(np.int)
-    return np.linspace(
-        np.min(not_nan_position), np.max(not_nan_position), n_bins + 1,
-        endpoint=True)
+def get_bin_edges(x, n_bins=None, bin_size=None):
+    not_nan_x = x[~np.isnan(x)]
+    if bin_size is not None:
+        n_bins = (
+            np.round(np.ceil(np.ptp(not_nan_x) / bin_size))).astype(np.int)
+    return np.linspace(np.min(not_nan_x), np.max(not_nan_x), n_bins + 1,
+                       endpoint=True)
 
 
 def get_bin_centers(bin_edges):
