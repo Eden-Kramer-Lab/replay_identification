283ad93c66060a1d4a1763504ddc66b89cacccad
Eric Denovellis <edeno@bu.edu>
2019-08-15 19:51:25 -0400

    Add KDE

--- replay_identification/multiunit_likelihood.py
+++ replay_identification/multiunit_likelihood.py
@@ -3,10 +3,14 @@
 
 from functools import partial
 
+import numba
 import numpy as np
+from sklearn.base import BaseEstimator, DensityMixin
 from tqdm.autonotebook import tqdm
 
 from .core import atleast_2d
+
+SQRT_2PI = np.float64(np.sqrt(2.0 * np.pi))
 
 
 def multiunit_likelihood(multiunit, position, place_bin_centers,
@@ -348,3 +352,38 @@ def fit_multiunit_likelihood(position, multiunit, is_training,
         marginal_models=marginal_models,
         mean_rates=mean_rates
     )
+
+
+@numba.njit(nogil=True, cache=True)
+def numba_kde(eval_points, samples, bandwidths):
+    n_eval_points, n_bandwidths = eval_points.shape
+    result = np.zeros((n_eval_points,))
+    n_samples = len(samples)
+
+    for i in range(n_eval_points):
+        for j in range(n_samples):
+            product_kernel = 1.0
+            for k in range(n_bandwidths):
+                bandwidth = bandwidths[k]
+                eval_point = eval_points[i, k]
+                sample = samples[j, k]
+                product_kernel *= (np.exp(
+                    -0.5 * ((eval_point - sample) / bandwidth)**2) /
+                    (bandwidth * SQRT_2PI)) / bandwidth
+            result[i] += product_kernel
+        result[i] /= n_samples
+
+    return result
+
+
+class NumbaKDE(BaseEstimator, DensityMixin):
+    def __init__(self, bandwidth=1.0):
+        self.bandwidth = bandwidth
+
+    def fit(self, X, y=None, sample_weight=None):
+        self.training_data = X
+        return self
+
+    def score_samples(self, X):
+        return np.log(numba_kde(X, self.training_data,
+                                self.bandwidth[-X.shape[1]:]))
