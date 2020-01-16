3ad15ab7cfbdfd3fb9d2ad8c495b0942cd4cbc40
Eric Denovellis <edeno@bu.edu>
2020-01-15 20:33:52 -0800

    Add nodejs to dependencies.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 INSTALL_REQUIRES = ['numpy >= 1.11', 'statsmodels', 'numba', 'matplotlib',
                     'xarray', 'scipy', 'scikit-learn', 'regularized_glm',
                     'spectral_connectivity', 'tqdm', 'joblib', 'networkx',
-                     'ipywidgets']
+                    'ipywidgets', 'nodejs']
 TESTS_REQUIRE = ['pytest >= 2.7.1']
 
 setup(
