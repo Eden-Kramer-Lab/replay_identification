e408d291b36ad85b3412d88f8b40b604bd2457f8
Eric Denovellis <edeno@bu.edu>
2022-03-17 16:20:57 -0700

    Add ClusterlessDetector

--- replay_identification/__init__.py
+++ replay_identification/__init__.py
@@ -1,6 +1,7 @@
 # flake8: noqa
 from replay_identification.decoders import ReplayDetector
-from replay_identification.detectors import SortedSpikesDetector
+from replay_identification.detectors import (ClusterlessDetector,
+                                             SortedSpikesDetector)
 from track_linearization import (get_linearized_position,
                                  make_actual_vs_linearized_position_movie,
                                  make_track_graph, plot_graph_as_1D,
