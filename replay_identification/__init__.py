a5830f06843dc6fe5a1efc09151064b9df39c3d4
Eric Denovellis <edeno@bu.edu>
2022-03-16 17:42:06 -0700

    Import detector

--- replay_identification/__init__.py
+++ replay_identification/__init__.py
@@ -1,7 +1,7 @@
 # flake8: noqa
+from replay_identification.decoders import ReplayDetector
+from replay_identification.detector import SortedSpikesDetector
 from track_linearization import (get_linearized_position,
                                  make_actual_vs_linearized_position_movie,
                                  make_track_graph, plot_graph_as_1D,
                                  plot_track_graph)
-
-from .decoders import ReplayDetector
