9018a14bdc7ecb1337b73dbefd5f30d5ef6c55d3
Eric Denovellis <edeno@bu.edu>
2022-10-06 11:35:32 -0400

    Bump version number

--- setup.py
+++ setup.py
@@ -2,20 +2,31 @@
 
 from setuptools import find_packages, setup
 
-INSTALL_REQUIRES = ['numpy >= 1.11', 'statsmodels', 'numba', 'matplotlib',
-                    'xarray', 'scipy', 'scikit-learn', 'regularized_glm',
-                    'spectral_connectivity', 'tqdm', 'joblib', 'networkx',
-                    'track_linearization']
-TESTS_REQUIRE = ['pytest >= 2.7.1']
+INSTALL_REQUIRES = [
+    "numpy >= 1.11",
+    "statsmodels",
+    "numba",
+    "matplotlib",
+    "xarray",
+    "scipy",
+    "scikit-learn",
+    "regularized_glm",
+    "spectral_connectivity",
+    "tqdm",
+    "joblib",
+    "networkx",
+    "track_linearization",
+]
+TESTS_REQUIRE = ["pytest >= 2.7.1"]
 
 setup(
-    name='replay_identification',
-    version='0.9.11.dev0',
-    license='MIT',
-    description=('Identify replay events using multiple information sources'),
-    author='Eric Denovellis',
-    author_email='edeno@bu.edu',
-    url='https://github.com/Eden-Kramer-Lab/replay_identification',
+    name="replay_identification",
+    version="0.9.12.dev0",
+    license="MIT",
+    description=("Identify replay events using multiple information sources"),
+    author="Eric Denovellis",
+    author_email="edeno@bu.edu",
+    url="https://github.com/Eden-Kramer-Lab/replay_identification",
     packages=find_packages(),
     install_requires=INSTALL_REQUIRES,
     tests_require=TESTS_REQUIRE,
